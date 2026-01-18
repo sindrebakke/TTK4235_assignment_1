3e5e14054ec426507edf310a6ed31f9ea7bcf070
sindre <sindrejbakke@gmail.com>
2026-01-18 14:08:15 +0100

    done

---
+++ source/new_file.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+    printf("Welcome to TTK4235 - Embedded Systems\n");
+    printf("Please be aware of the fact that most animals, especially ferrets, are not allowed in Sanntidssalen.\n");
+    printf("Smaller birds, e.g. pigeons, may in some very rare cases be permitted.\n");
+
+    return 0;
+}
